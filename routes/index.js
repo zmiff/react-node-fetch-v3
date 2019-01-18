bd55b2f8e5ea21ec4a5dc4083c90d2625ec39243
Ugur Tamer <ugur_tamer@hotmail.com>
2019-01-18 23:55:32 +0100

    went up 1 dir '..'

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 var express = require('express');
 const app = express();
 var router = express.Router();
-const publicPath = path.join(__dirname, '..', 'client', 'build');
+const publicPath = path.join(__dirname, '..', '..', 'client', 'build');
 app.use(express.static(publicPath));
 
 /* GET home page. */
