59a3ca930c149feafcfb8bc01b348c7926452a40
Ugur Tamer <ugur_tamer@hotmail.com>
2019-01-18 23:58:00 +0100

    added build to git

---
+++ client/build/precache-manifest.a2c5ea8b4c892d07663390b6110cbdee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1579634ccb536710a233",
+    "url": "/static/js/main.1579634c.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "1579634ccb536710a233",
+    "url": "/static/css/main.e9829dd7.chunk.css"
+  },
+  {
+    "revision": "24706a49a135b34f422c7b33530d8272",
+    "url": "/index.html"
+  }
+];
