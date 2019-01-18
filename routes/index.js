57dcb80e8821c3a0a1c2a22d7ef764c6b5ffe7b1
Ugur Tamer <ugur_tamer@hotmail.com>
2019-01-18 23:52:06 +0100

    added publicpath and express.static to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+const path = require('path');
 var express = require('express');
+const app = express();
 var router = express.Router();
+const publicPath = path.join(__dirname, '..', 'client', 'build');
+app.use(express.static(publicPath));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('/client/build/index');
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 module.exports = router;
